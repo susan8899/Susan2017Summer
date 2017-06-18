594a87d7999bada73790e42183a2d0011aca36a7
Susan Carrera <SusanCarrera@SusanCaerasMBP2.attlocal.net>
2017-06-18 18:11:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/susancarrera/susan2017summer/NewActivity.java
@@ -0,0 +1,17 @@
+package com.example.susancarrera.susan2017summer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SusanCarrera on 6/17/17.
+ */
+
+public class NewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
