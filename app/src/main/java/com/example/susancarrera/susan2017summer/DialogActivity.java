15cb061630b5cca22fab8334fb74c5fc9d18d682
SusanCarrera <su.susan89@gmail.com>
2017-07-03 09:45:24 -0400

    BEFORE QUIZ 3

--- app/src/main/java/com/example/susancarrera/susan2017summer/DialogActivity.java
+++ app/src/main/java/com/example/susancarrera/susan2017summer/DialogActivity.java
@@ -1,10 +1,15 @@
 package com.example.susancarrera.susan2017summer;
 
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
 import android.support.annotation.IdRes;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
+
+import com.example.susancarrera.susan2017summer.dialog.CustomDialog;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,6 +26,7 @@ public class DialogActivity extends BaseActivity {
         switch (checkedID){
 
             case R.id.activity_dialog_rb1:
+                    normalDialog();
                 break;
             case R.id.activity_dialog_rb2:
                 break;
@@ -36,6 +42,15 @@ public class DialogActivity extends BaseActivity {
                 break;
             case R.id.activity_dialog_rb8:
 
+                CustomDialog customDialog =
+                        new CustomDialog(this, new CustomDialog.ICustomDialogListener() {
+                    @Override
+                    public void onOKClicked(String msg) {
+                        showToast(msg);
+                    }
+                });
+                customDialog.setCanceledOnTouchOutside(true);
+                customDialog.show();
 ;                break;
             default:
         }
@@ -52,10 +67,28 @@ public class DialogActivity extends BaseActivity {
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                checkedID = checkedId;
                 shortToast("you chose: " +checkedId);
 
 
             }
         });
     }
+
+        private void normalDialog(){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setIcon(R.mipmap.ic_launcher);
+            builder.setTitle("AlertTitle");
+            builder.setMessage("This is a normal Dialog");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int which){
+                    shortToast("You clicked Yes");
+                }
+            });
+
+
+
+        }
+
 }
--- app/src/main/java/com/example/susancarrera/susan2017summer/dialog/CustomDialog.java
+++ app/src/main/java/com/example/susancarrera/susan2017summer/dialog/CustomDialog.java
@@ -3,16 +3,41 @@ package com.example.susancarrera.susan2017summer.dialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.View;
 
 import com.example.susancarrera.susan2017summer.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by SusanCarrera on 7/3/17.
  */
 
 public class CustomDialog extends Dialog {
-    public CustomDialog(@NonNull Context context){
-        super(context);
+
+
+    private final ICustomDialogListener listener;
+
+    public interface ICustomDialogListener{
+        public void onOKClicked(String msg);
+        //abstract void onCancleClicked();
+
+    }
+
+
+    @OnClick(R.id.dialog_custom_ok)
+    public void ok(View view){
+        listener.onOKClicked("You clicked OK");
+
+        cancel();
+    }
+
+
+    public CustomDialog(@NonNull Context context,ICustomDialogListener listener){
+        super(context,R.style.dialog);
         setContentView(R.layout.dialog_custom);
+        ButterKnife.bind(this);
+        this.listener = listener;
     }
 }
